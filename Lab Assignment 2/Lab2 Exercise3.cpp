265bf4890139a01cf9517c107775e418febf772f
Eliezer312 <68049296+Eliezer312@users.noreply.github.com>
2020-12-16 23:03:22 +0800

    Add files via upload

---
+++ Lab Assignment 2/Lab2 Exercise3.cpp
@@ -0,0 +1,104 @@
+/*Eliezer binti Maingkin
+BI19110061
+Lab Assignment 2 - Exercise 3 */
+
+#include <iostream>
+
+using namespace std;
+
+class POSNode{
+	string item;
+	float price;
+	POSNode *next;
+
+	public:
+	POSNode(){};
+	
+void addItem (string newItem){
+	item = newItem;
+};
+
+void itemPrice (float newPrice){
+	price = newPrice;
+};
+
+void setNext (POSNode *addNext){ 
+	next = addNext; 
+};
+
+string getItem (){
+	return item; 
+};
+
+float getPrice (){ 
+	return price; 
+};
+
+POSNode *Next (){ 
+	return next; 
+};
+};
+
+class POSList{
+	POSNode *head;
+
+	public:
+	POSList (){ 
+		head = NULL; 
+	};
+
+void addNode(string item, float price);
+void print();
+};
+
+void POSList::addNode(string item, float price){
+	POSNode *newNode = new POSNode();
+	newNode->addItem(item);
+	newNode->itemPrice(price);
+	newNode->setNext(NULL);
+	POSNode *tmp = head;
+	
+	if (tmp != NULL) {
+		while (tmp->Next() != NULL) {
+		tmp = tmp->Next();
+		}
+	tmp->setNext(newNode);
+	}
+	
+	else {
+	head = newNode;
+	}
+}
+
+void POSList::print(){
+	POSNode *p = head;
+	int i = 10;
+	float sum = 0.0;
+	cout << "==============================================" << endl;
+	cout << "BC	ITEMS				PRICE" << endl;
+	cout << "==============================================" << endl;
+	
+	while (p->Next() != NULL) {
+	cout << i << "	" << p->getItem() << " " << p->getPrice() << endl;
+	i++;
+	sum = sum + p->getPrice();
+	p = p->Next();
+	}
+	
+	sum = sum + p->getPrice();
+	cout << i << "	" << p->getItem() << "" << p->getPrice() << endl;
+	cout << "==============================================="<< endl;
+	cout << "Total (GST Incl.)		       "<< sum<< endl;
+	cout << "==============================================="<< endl;
+}
+
+int main(){
+	POSList list;
+	list.addNode("Pagoda Gnut 110g               ", 3.49);
+	list.addNode("Hup Seng Cream Cracker         ", 4.19);
+	list.addNode("Yit Poh 2n1 Kopi-o             ", 7.28);
+	list.addNode("Zoelife SN & Seed              ", 5.24);
+	list.addNode("Gatsby S/FO Wet&Hard	      ", 16.99);
+	list.addNode("GB W/G U/Hold 150g              ", 6.49);
+	list.print();
+}
