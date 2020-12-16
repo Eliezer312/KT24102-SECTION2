7928d24f1b506a5c0f784b88bc78097c9b5b1fab
Eliezer312 <68049296+Eliezer312@users.noreply.github.com>
2020-12-16 23:42:51 +0800

    Add files via upload

---
+++ Lab Assignment 3/Lab3 Part1 Exercise2.cpp
@@ -0,0 +1,50 @@
+/*Eliezer binti Maingkin
+BI19110061
+Lab Assignment 3 - Part 1 Exercise 2 */
+
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+bool pairSymbol(char opening,char closing)
+{
+	if(opening == '(' && closing == ')') 
+		return true;
+	else if(opening == '{' && closing == '}') 
+		return true;
+	else if(opening == '[' && closing == ']')
+		return true;
+	else
+		return false;
+}
+
+bool paranthesisBalanced(string symbol)
+{
+	stack<char> S;
+	for(int i =0;i<symbol.length();i++)
+	{
+		if(symbol[i] == '(' || symbol[i] == '{' || symbol[i] == '[')
+			S.push(symbol[i]);
+		else if(symbol[i] == ')' || symbol[i] == '}' ||symbol[i] == ']')
+		{
+			if(S.empty() || !pairSymbol(S.top(),symbol[i]))
+				return false;
+			else
+				S.pop();
+		}
+	}
+	return S.empty() ? true:false;
+}
+
+int main(){	
+    string inputSymbol;
+	cout<<"Enter an expression of parenthesis symbols:  ";
+	cin>> inputSymbol;
+	if(paranthesisBalanced(inputSymbol))
+		cout<<"True\n";
+	else
+		cout<<"False\n";
+    return 0;
+}
